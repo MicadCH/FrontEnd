43e8cbcda1bc6abc0c8f7c778bb53e5994ef5915
Micaela Dana Chervet <106720650+MicadCH@users.noreply.github.com>
2022-09-25 23:52:20 -0300

    Corrigiendo el ScrollToTop

--- src/app/components/scrolltop/scrolltop.component.ts
+++ src/app/components/scrolltop/scrolltop.component.ts
@@ -6,35 +6,39 @@ import { Scroll } from '@angular/router';
 @Component({
   selector: 'app-scrolltop',
   templateUrl: './scrolltop.component.html',
-  styleUrls: ['./scrolltop.component.css']
+  styleUrls: ['./scrolltop.component.css'],
 })
 export class ScrolltopComponent implements OnInit {
-
   windowScrolled?: boolean;
 
-  constructor(@Inject(DOCUMENT) private document: Document) { }
+  constructor(@Inject(DOCUMENT) private document: Document) {}
 
   @HostListener('window:scroll', [])
-  onWindowScroll(): void{
-    if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100){
+  onWindowScroll(): void {
+    if (
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop > 100
+    ) {
       this.windowScrolled = true;
-    }
-    else if (this.windowScrolled && window.pageYOffset || this.document.documentElement.scrollTop || document.body.scrollTop < 10){
+    } else if (
+      (this.windowScrolled && window.pageYOffset) ||
+      this.document.documentElement.scrollTop ||
+      document.body.scrollTop < 10
+    ) {
       this.windowScrolled = false;
     }
   }
 
   scrollToTop(): void {
     (function smoothscroll(): void {
-        const currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
-        if (currentScroll > 0) {
-            window.requestAnimationFrame(smoothscroll);
-            window.scrollTo(0, currentScroll - (currentScroll * 8));
+      const currentScroll =
+        document.documentElement.scrollTop || document.body.scrollTop;
+      if (currentScroll > 0) {
+        window.scrollTo({ top: 0 , behavior: "smooth"});
       }
     })();
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
