1e81646fa39e1c9cda90f0bdf166dc4348200370
Micaela Dana Chervet <106720650+MicadCH@users.noreply.github.com>
2022-10-30 02:47:59 -0300

    e

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditeducacionComponent } from './components/educacion/editeducacion.component';
 import { NeweducacionComponent } from './components/educacion/neweducacion.component';
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 import { HomeComponent } from './components/home/home.component';
-
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
@@ -13,6 +13,7 @@ const routes: Routes = [
   { path: 'nuevaexp', component: NewExperienciaComponent},
   { path: 'editexp/:id', component: EditExperienciaComponent},
   { path: 'nuevaedu', component: NeweducacionComponent},
+  { path: 'editedu/:id', component: EditeducacionComponent},
 ];
 
 @NgModule({
